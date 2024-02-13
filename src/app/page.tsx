f59e31d42fbc9364c32b0d62ecdec1f761b47ed7
Jaromír Trnka <JrTrnka55@gmail.com>
2024-02-13 19:59:32 +0100

    tutorial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,13 @@ import Header from "@/components/header";
 export default function Home() {
     return (
         <>
-            <Header />
-            <main></main>
+            <div className="grid grid-cols-[inherit] [grid-column:page] max-md:landscape:hidden">
+                <Header />
+                <main></main>
+            </div>
+            <div className="hidden min-h-[100svh] items-center justify-center [grid-column:content] max-md:landscape:flex">
+                <span>Use portrait orientation.</span>
+            </div>
         </>
     );
 }
