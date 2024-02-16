aedf4c6536176ac07fd868ab3f9385a7df9c1c07
Jaromír Trnka <JrTrnka55@gmail.com>
2024-02-16 16:23:18 +0100

    login screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,17 @@
 "use client";
 
-import {useSession} from "next-auth/react";
-import {Dashboard} from "@/components/dashboard/Dashboard";
-import {Homepage} from "@/components/homepage/Homepage";
+import { useSession } from "next-auth/react";
+import { Dashboard } from "@/components/dashboard/Dashboard";
+import { Homepage } from "@/components/homepage/Homepage";
 
 export default function Home() {
     const { data, status } = useSession();
 
     return (
-        <main className="[grid-column:_content]">
-            {status === 'authenticated' ? (
-                <Dashboard/>
-            ) : (
-                <Homepage/>
-            )}
+        <main className="mt-12 [grid-column:_content]">
+            {status === "authenticated" ?
+                <Dashboard />
+            :   <Homepage />}
         </main>
     );
 }
