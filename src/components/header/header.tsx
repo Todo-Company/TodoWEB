f5e5159f8217563dfa3da9c26c31db5d28f47395
Jaromír Trnka <JrTrnka55@gmail.com>
2024-02-24 18:49:33 +0100

    Update header.tsx

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -222,7 +222,6 @@ export default function Header() {
                             </div>
                         </>
                     )}
-
                     <ModeToggle />
 
                     <User />
