f5f62c771c14636d6fb295777dc6039ce04f7c9e
Jaromír Trnka <JrTrnka55@gmail.com>
2024-02-24 18:44:20 +0100

    slide in component for on load effect

--- src/components/homepage/Homepage.tsx
+++ src/components/homepage/Homepage.tsx
@@ -1,13 +1,17 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import React from "react";
+import SlideIn from "@/components/ui/slideIn";
 
 export function Homepage() {
     return (
         <div className="relative grid grid-cols-[inherit] [grid-column:page]">
-            <div className="absolute z-0 h-[calc(100svh_-_74px)] w-full overflow-clip [grid-column:_page] [mask-image:radial-gradient(hsl(var(--background)),_transparent_80%)]">
+            <SlideIn className="absolute z-0 h-[calc(100svh_-_74px)] w-full overflow-clip [grid-column:_page] [mask-image:radial-gradient(hsl(var(--background)),_transparent_80%)] ">
                 <div className="absolute -inset-2 h-[200svh] w-[200vw] -translate-x-1/4 -translate-y-1/4 skew-x-[-25deg] skew-y-[-2deg] bg-[linear-gradient(0deg,var(--c)var(--t),transparent_.1px_100%),linear-gradient(90deg,var(--c)var(--t),transparent_.1px_100%)] bg-[size:var(--s)_var(--s)] antialiased [--c:_hsl(var(--todoFinished-foreground))] [--s:_30px] [--t:_1px] sm:[--s:_35px] md:[--s:_40px] lg:skew-x-[-32deg] lg:[--s:_45px] xl:[--s:_50px]"></div>
-            </div>
-            <div className="relative z-10 mt-12 grid h-fit max-w-[60ch] py-20 [grid-column:_content] ">
+            </SlideIn>
+            <SlideIn className="relative z-10 mt-12 grid h-fit max-w-[60ch] py-20 delay-100 [grid-column:_content]">
                 <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight drop-shadow-xl first:mt-0 ">
                     It would seem like you are not logged in!
                 </h2>
@@ -18,7 +22,7 @@ export function Homepage() {
                 <Link href={"/login"} className="mt-8 w-fit">
                     <Button variant={"default"}>Log In</Button>
                 </Link>
-            </div>
+            </SlideIn>
         </div>
     );
 }
