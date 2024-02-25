c6809cf2636cdba595ca1e434d62f1fa4f67f45d
Jaromír Trnka <JrTrnka55@gmail.com>
2024-02-25 16:57:02 +0100

    footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { useSession } from "next-auth/react";
 import { Dashboard } from "@/components/dashboard/Dashboard";
 import { Homepage } from "@/components/homepage/Homepage";
+import FooterComponent from "@/components/footer/footer";
 
 export default function Home() {
     const { data, status } = useSession();
@@ -10,7 +11,10 @@ export default function Home() {
     return (
         <main className="grid grid-cols-[inherit] [grid-column:_page]">
             {status === "authenticated" ?
-                <Dashboard />
+                <>
+                    <Dashboard />
+                    <FooterComponent />
+                </>
             :   <Homepage />}
         </main>
     );
