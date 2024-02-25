eea31a4c87af5181a5678f2b4a609dd26dc83dd9
Jaromír Trnka <JrTrnka55@gmail.com>
2024-02-25 16:58:47 +0100

    mode toggle whole button register click

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -7,18 +7,12 @@ import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
 
 export function ModeToggle() {
-    const { setTheme } = useTheme();
+    const { theme, setTheme } = useTheme();
 
     return (
-        <Button variant="outline" size="icon">
-            <Sun
-                onClick={() => setTheme("dark")}
-                className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-            />
-            <Moon
-                onClick={() => setTheme("light")}
-                className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-            />
+        <Button variant="outline" size="icon" onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
+            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
         </Button>
     );
 }
